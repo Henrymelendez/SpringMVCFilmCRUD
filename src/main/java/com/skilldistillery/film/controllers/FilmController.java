e465a346b43e3f968985b8b2186024726bf47e4b
Henrymelendez <henryomarm@gmail.com>
2022-05-06 23:39:53 -0400

    added more bootstrap and a controller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -2,11 +2,14 @@ package com.skilldistillery.film.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.film.dao.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
@@ -21,15 +24,31 @@ public class FilmController {
 		return "home";
 	}
 	
-	@RequestMapping(path ="lookup.do", method = RequestMethod.GET)
-	public ModelAndView formSelector() {
+	@RequestMapping(path = {"/","form.do"})
+	public String addFilmForm() {
 		
+		return "addFilm";
+	}
 	
+	//adds film from addFilm.jsp
+	@RequestMapping(path ="add.do", method = RequestMethod.POST)
+	public ModelAndView addFilm(Film film, RedirectAttributes redir) {
 		ModelAndView mv = new ModelAndView();
+		filmDao.createFilm(film);
+		redir.addFlashAttribute("addingObject", film);
+		mv.setViewName("redirect:filmAdded.do");
 		
 		
 		return mv;
 	}
 //use redirect when adding/deleting/editing film
+	
+	// redirect when film is added 
+	@RequestMapping(path = "filmAdded.do")
+	public ModelAndView createdFilm() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("result");
+		return mv;
+	}
 
 }
--- src/main/webapp/WEB-INF/views/addFilm.jsp
+++ src/main/webapp/WEB-INF/views/addFilm.jsp
@@ -12,37 +12,37 @@
 <title>Insert title here</title>
 </head>
 <body>
-
-<form class="needs-validation" novalidate>
+<div class="d-flex justify-content-center">
+<form class="needs-validation col-lg-6" action="add.do" id="addFilm" method="POST" novalidate>
 
 <div class="form-row">
 
     <div class="col-md-4 mb-3">
       <label for="validationTooltip01">Add Film Title</label>
-      <input type="text" class="form-control" name="" id="validationTooltip01" placeholder="First name"  required>
-      <div class="valid-tooltip">
-        Looks good!
+      <input type="text" class="form-control" name="" id="validationTooltip01" placeholder="Title"  required>
+      <div class="invalid-tooltip">
+        Add a Title
       </div>
+    </div>
     </div>
     
     
-    
-    
+    <div class="form-row">
     <div class="form-group">
     <label for="exampleFormControlTextarea1">Film Description</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="" required></textarea>
-    <div class="valid-tooltip">
+    <div class="invalid-tooltip">
     	Add a Description
     </div>
   </div>
-  
-  
+	
+  </div>
     
     
     
     
 </form>
-
+</div>
 
 </body>
 </html>
